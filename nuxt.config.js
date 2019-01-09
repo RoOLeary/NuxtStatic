1fd1f988d11b5c15eed75c45c0c0b79eabfd26cb
Ronan <roleary81@gmail.com>
2019-01-09 23:23:04 +0100

    deploy dist

---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~/assets/css/tailwind.css',
+    '~/assets/main.css',
+    'aos/dist/aos.css',
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: "~/plugins/aos", ssr: false },
+    { src: "~/plugins/owl.js", ssr: false}
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+ 
+  router: {
+    base: '/NuxtStatic/'
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+
+    */
+    extractCSS: true,
+   
+    extend (config, { isDev, isClient }) {
+      if (isDev && process.client) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+        }
+      }
+  },
+}
