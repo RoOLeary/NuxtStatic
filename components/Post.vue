d1f5e68aa96890f67d7c467d9ff2e977995b4e65
Ronan <roleary81@gmail.com>
2019-01-12 04:42:46 +0100

    fix post single

--- components/Post.vue
+++ components/Post.vue
@@ -15,10 +15,10 @@
                 <p class="text-grey-darker text-base">{{ body }}</p>
                 </div>
                 <div class="flex items-center">
-                <img class="w-10 h-10 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg" alt="Avatar of Jonathan Reinink">
+                <img class="w-10 h-10 rounded-full mr-4" src="https://ronan-oleary.com/wp/wp-content/uploads/2017/06/rosenberg.png" alt="Avatar of Jonathan Reinink">
                 <div class="text-sm">
-                    <p class="text-black leading-none">Jonathan Reinink</p>
-                    <p class="text-grey-dark">Aug 18</p>
+                    <p class="text-black leading-none">Danger Ro</p>
+                    <p class="text-grey-dark">Jan 12</p>
                 </div>
                 </div>
                 <nuxt-link :to="path">
