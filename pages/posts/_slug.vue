c485f834488013a93f21b4e196aaa16498e6675e
Ronan <roleary81@gmail.com>
2019-01-12 10:15:02 +0100

    tidy up single post view

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="container">
-    <div class="single-post text-center">
-        <h1>{{ title }}</h1>
+  <div>
+    <no-ssr>
+      <div class="container mx-auto postsindex mt-32">
+        <h1 class="text-5xl">{{ title }}</h1>
+        <br>
+        <p><img src="https://placehold.it/550x350" alt="placeholder" style="width: 760px"/></p>
         <p><strong>{{ date }}</strong></p>
-        <p>{{ body }}</p>
-        <nuxt-link to='/posts'>Back to Posts</nuxt-link>
-    </div>
+        <div class="flex flex-wrap">
+            <div class="sm:w-full md:w-1/4 lg:w-1/4 xl:w-1/4 mb-2 py-8"></div> 
+            <div class="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 mb-2 p-8 text-left">
+                <p class="leading-normal">{{ body }}</p>
+                <br>
+                <nuxt-link to='/posts' class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded no-underline">Back to Posts</nuxt-link>  
+            </div> 
+            <div class="sm:w-full md:w-1/4 lg:w-1/4 xl:w-1/4 mb-2 p-8"></div> 
+          </div>
+      </div>
+    </no-ssr>
   </div>
 </template>
 
@@ -24,13 +35,12 @@ export default {
 <style scoped>
 
   .container{
-      background: black;
+      background: #fff;
       min-height: 100vh;
       min-width: 100vw;
   }
 
-  h1, p, a{
-    color: #fff;
+  h1, a{
     font-weight: bold;
   }
   .single-post{
