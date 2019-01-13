2ffd2ed220af85c382f2b1fd2189328a71d98776
Ronan <roleary81@gmail.com>
2019-01-13 13:16:14 +0100

    minor addition to global footer

--- components/Globalfooter.vue
+++ components/Globalfooter.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <footer class="w-full pin-b text-center bg-black  border-t border-grey p-4">
-           <p class="text-white">&copy; {{ year }} {{ credit }} </p>
-           <p class="text-white">Made with <span>&#9825;</span> for boobs and <a href="https://vuejs.org/" target="_blank" class="text-red hover:text-white" rel="nofollow noopener">VueJS</a> in Amsterdam</p>
+            <p class="text-white">&copy; {{ year }} {{ credit }} </p>
+            <p class="text-white">Made with <span>&#9825;</span> and <a href="https://vuejs.org/" target="_blank" class="text-red hover:text-white" rel="nofollow noopener">VueJS</a> in Amsterdam</p>
+            <p class="text-white"><a href="https://liverpoolfc.com" target="_blank" rel="nofollow noopener" class="text-red no-underline">YNWA</a></p>
         </footer>
     </div>
 </template>
